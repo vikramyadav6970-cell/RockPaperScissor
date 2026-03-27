5507c7a3fc0f868425070ee792446e7a6c444fbf
vikramyadav6970-cell <vikramyadav6970@gmail.com>
2026-03-27 13:29:33 +0530

    Added new functionality

--- index.js
+++ index.js
@@ -1,15 +1,40 @@
-let start = document.getElementById("btn")
-let image = document.getElementById("image")
-let text = document.getElementById("text")
+const start = document.getElementById("btn")
+const image = document.getElementById("image")
+const text = document.getElementById("text")
+const selectEl = document.getElementById("drop-down")
+const playerImage = document.getElementById("player-img")
+const playerText = document.getElementById("player-text")
+const message = document.getElementById("message")
 
 let hands = ["rock", "paper", "scissor"]
+let playerChoice
+
+function Player(){
+    message.textContent = "Let's Play"
+    image.src=""
+
+    playerChoice = selectEl.value
+
+    if(playerChoice=="rock"){
+        playerImage.src="images/rock.png"
+        playerText.textContent="Rock"
+    }
+    else if(playerChoice=="paper"){
+        playerImage.src="images/paper.png"
+        playerText.textContent="Paper"
+    }
+    else{
+        playerImage.src="images/scissor.png"
+        playerText.textContent="Scissor"
+    }
+}
 
 function play(){
     let randomNumber = Math.floor(Math.random()*3)
     return hands[randomNumber]
 }
 
-function playGame(){
+function showComputerImage(){
     let move = play()
     if(move=="rock"){
         image.src="images/rock.png"
@@ -23,7 +48,20 @@ function playGame(){
         image.src="images/scissor.png"
         text.textContent="Scissor"
     }
+
+    return move
+}
+
+function playGame(){
+    if(showComputerImage() == "rock" && playerChoice == "paper" || showComputerImage() == "scissor" && playerChoice == "rock" || showComputerImage() == "paper" && playerChoice == "scissor")
+        message.textContent = "Hurray You Won"
+
+    else if(showComputerImage() == playerChoice)
+        message.textContent = "Game Tie"
+
+    else
+        message.textContent = "Computer wins"
 }
 
 start.addEventListener("click",playGame)
-
+selectEl.addEventListener("change",Player)
